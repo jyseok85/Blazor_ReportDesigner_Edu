ba62d8779493e4611a656ee7feea82466024bf12
연석 조 <jyseok@duzonbizon.onmicrosoft.com>
2023-07-05 16:33:40 +0900

    초기 커밋.

---
+++ Steps/01.DragNDrop/Model/_01_DragAndDropService.cs
@@ -0,0 +1,11 @@
+﻿public class _01_DragAndDropService
+{
+    public int PosX { get; set; }
+    public int PosY { get; set; }
+    public void StartDrag(int posX, int posY)
+    {
+        this.PosX = posX;
+        this.PosY = posY;
+    }
+}
+
